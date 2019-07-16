379d77054be1b130ff0ba88adbbf264fb50c4bf6
Richard Lawson <lawson.a.richard@gmail.com>
2019-07-16 15:11:48 -0700

    buttons show proper numbers/characters working on styling

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,18 +1,20 @@
-import React, { useState } "react";
-
+import React, { useState } from "react";
+import NumberButton from "./NumberButton";
 //import any components needed
-import {numbers} from "../../../data.js";
+import { numbers } from "../../../data";
 //Import your array data to from the provided data file
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
-  const [numbers, setNumber] = useState(numbers);
+  const [numberState, setNumberState] = useState(numbers);
   return (
-    <div>
-      
+    <div class="numbersButton">
+  
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {numbers.map((number) => NumberButton (number))}
     </div>
   );
 };
+export default Numbers;
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,12 @@
 import React from "react";
-
-const SpecialButton = () => {
+import Specials from "./Specials";
+const SpecialButton = (specials) => {
   return (
+
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button>{specials}</button>
     </>
   );
 };
+export default SpecialButton;
